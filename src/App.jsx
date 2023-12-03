77eecb9c382acd94c3c272ab7f5cb7b11778afc4
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-03 14:21:45 +0200

    refactor useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
+
 import { useState } from "react"
 
 function App() {
 
   const [count, setCount] = useState(0)
-  function Increment(){
-    setCount(count+1)
-  }
-
   return (
-    <div className="flex justify-center items-center mt-10 gap-2">
-        <h1 className='text-2xl'>{count}</h1>
-        <button className="border border-gray-400 rounded-xl px-2" onClick={Increment}>Increment</button>
-    </div>
-
-    
-  )
+    <section className="flex justify-center items-center h-screen">
+      <span className='text-2xl pb-24'>{count}</span>
+      <div className="flex gap-2">
+        <button className="border border-gray-400 rounded-xl px-2 bg-gray-200" onClick={()=>setCount(count-1)}>Decrement</button>
+        <button className="border border-gray-400 rounded-xl px-2 bg-gray-200" onClick={()=>setCount(count+1)}>Increment</button>
+      </div>
+    </section>
+  );
 }
 
 export default App
