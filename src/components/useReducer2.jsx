44c7ea0ee34ea539465f3c96562acc76670c18df
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-04 23:04:39 +0200

    refactor use reducer

--- src/components/useReducer2.jsx
+++ src/components/useReducer2.jsx
@@ -1,17 +1,16 @@
 import { useReducer} from "react"
 
-export default function UseReducer(){
-    function reducer(state,action){
-        switch(action.type){
-            case "Increment":
-            return{count:state.count+1, showText:state.showText}
-            case"ShowText":
-            return{count:state.count, showText:!state.showText}
-            default:
-                return state
-        }
-
+function reducer(state,action){
+    switch(action.type){
+        case "Increment":
+        return{count:state.count+1, showText:state.showText}
+        case"ShowText":
+        return{count:state.count, showText:!state.showText}
+        default:
+            return state
     }
+}
+export default function UseReducer(){
 
     const [state, dispatch] = useReducer(reducer, {count:0, showText:false})
 
@@ -19,8 +18,8 @@ export default function UseReducer(){
       <section className="flex flex-col">
         <span>{state.count}</span>
         <button onClick={()=> {
-            dispatch({type: Increment})
-            dispatch({type: ShowText})
+            dispatch({type: "Increment"})
+            dispatch({type: "ShowText"})
         }}>Show Text</button>
         {state.showText&&<span>This is the Text</span>}
       </section>
